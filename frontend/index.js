2c21f7ad596ad33e132e3a440fba95ee2aa54efa
diandra <diandrarizqy7@gmail.com>
2023-10-11 16:09:55 +0000

    flylow scenario ok

--- frontend/index.js
+++ frontend/index.js
@@ -34,29 +34,41 @@ server.listen(port, () => {
 
 
 ///////////parse sensor data
-const sensor_datas = []
-
-fs.readFile('./public/datas/flylow_16_48_25_edit.csv', 'utf8', (err, data) => {
+const sensor_datas = [];
+const filename = 'flylow_16_48_25_edit';
+fs.readFile(`./public/datas/${filename}.csv`, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
 
     function processSensorData(data){
-        const NORMAL_DATA_LEN = 74;
+        const DATA_UAV_LEN = 7;
+        const DATA_SENSOR_LEN = 9;
+        const DATA_THERMAL_LEN = 64;
+        const DATA_FIRE_LEN = 1;
+        const NORMAL_DATA_LEN = DATA_UAV_LEN + DATA_SENSOR_LEN + DATA_THERMAL_LEN + DATA_FIRE_LEN;
         const data_arr = data.split(',');
         if(data_arr.length != NORMAL_DATA_LEN)            
             return null;//reject
 
-        const thermal_datas_arr = data_arr.slice(9 ,data_arr.length - 1);
+        const thermal_datas_arr = data_arr.slice(DATA_UAV_LEN + DATA_SENSOR_LEN,data_arr.length - 1);
         const thermal_datas = []
         thermal_datas_arr.forEach(thermal_d => {
             thermal_datas.push(parseFloat(thermal_d));
         });
 
-        var idx = -1
+        var idx = -1;
+        const latlon_mult = 1e-7;
         const payload = {
             time            : data_arr[++idx],
+            roll            : parseFloat(data_arr[++idx]),
+            pitch           : parseFloat(data_arr[++idx]),
+            yaw             : parseFloat(data_arr[++idx]),
+            altitude        : parseFloat(data_arr[++idx]),
+            latitude        : parseFloat(data_arr[++idx]) * latlon_mult,
+            longitude       : parseFloat(data_arr[++idx]) * latlon_mult,
+            airspeed        : parseFloat(data_arr[++idx]),
             cjmcu_co        : parseFloat(data_arr[++idx]), 
             bme_humidity    : parseFloat(data_arr[++idx]), 
             dust_density    : parseFloat(data_arr[++idx]), 
